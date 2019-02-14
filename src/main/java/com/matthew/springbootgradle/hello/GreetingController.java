ad5400e84cb990dd317b2becbbadb3524d2e88cc
MatthewLQM <matthewlqm@163.com>
2019-02-14 21:28:57 +0800

    使用SpringBoot的JDBC实现了数据库的查询功能

--- src/main/java/com/matthew/springbootgradle/hello/GreetingController.java
+++ src/main/java/com/matthew/springbootgradle/hello/GreetingController.java
@@ -1,9 +1,16 @@
 package com.matthew.springbootgradle.hello;
 
+import com.matthew.springbootgradle.dal.model.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -17,6 +24,9 @@ public class GreetingController {
 
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
@@ -24,6 +34,18 @@ public class GreetingController {
 
     @RequestMapping("/query")
     public String query() {
-        return "Not Enough";
+        String sql = "SELECT * FROM test_table";
+        List<Test> tests = jdbcTemplate.query(sql, (resultSet, rowNum) -> {
+            Test test = new Test();
+            test.setId(resultSet.getLong("id"));
+            test.setName(resultSet.getString("name"));
+            return test;
+        });
+        if(tests.isEmpty()) {
+            return "";
+        }
+        StringBuffer result = new StringBuffer();
+        tests.forEach(test -> result.append(test).append(";"));
+        return result.toString();
     }
 }
